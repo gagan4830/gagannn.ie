e85be52c2e6fada690be33a47253bf3b5dfaa008
unknown <gagan4830>
2019-02-15 09:17:16 +0530

    Initial commit

---
+++ ie.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+
+driver=webdriver.Ie("C:/Users/acer/Downloads/Compressed/IEDriverServer_Win32_3.14.0/IEDriverServer.exe")
+driver.get("file:///C:/Users/acer/Desktop/login.html")
